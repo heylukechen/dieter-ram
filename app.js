c303755a1708a7b94ed9575dc312058eac9761a8
heylukechen <lukechen0114@gmail.com>
2023-05-23 10:55:31 +0200

    for publishing

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res, next) => {
-  res.sendFile(__dirname + "/views/home.html");
+  res.sendFile(__dirname + "/views/home");
 });
 
 app.get("/about", (req, res, next) => {
-    res.sendFile(__dirname + "/views/about.html");
+    res.sendFile(__dirname + "/views/about");
 });
 
 app.get("/gallery", (req, res, next) => {
-    res.sendFile(__dirname + "/views/gallery.html");
+    res.sendFile(__dirname + "/views/gallery");
 });
 
 app.get("/resources", (req, res, next) => {
-    res.sendFile(__dirname + "/views/resources.html");
+    res.sendFile(__dirname + "/views/resources");
 });
 
 app.listen(3000, () => console.log("My first app listening on port 3000! "));
