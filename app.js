59f97db5a5c4e9612387b63b29985a775c8160fa
heylukechen <lukechen0114@gmail.com>
2023-05-23 11:10:23 +0200

    Clean up version

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ const app = express();
 
 app.use(express.static("public"));
 
-
-app.get("/home", (req, res, next) => {
-  res.sendFile(__dirname + "/views/home");
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/views/home.html");
 });
 
 app.get("/about", (req, res, next) => {
-    res.sendFile(__dirname + "/views/about");
+    res.sendFile(__dirname + "/views/about.html");
 });
 
 app.get("/gallery", (req, res, next) => {
-    res.sendFile(__dirname + "/views/gallery");
+    res.sendFile(__dirname + "/views/gallery.html");
 });
 
 app.get("/resources", (req, res, next) => {
-    res.sendFile(__dirname + "/views/resources");
+    res.sendFile(__dirname + "/views/resources.html");
 });
 
 app.listen(3000, () => console.log("My first app listening on port 3000! "));
